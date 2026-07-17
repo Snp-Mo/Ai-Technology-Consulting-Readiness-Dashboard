506565fac2b769c4a388ae7380c284a6c9ce0cfe
Snp-Mo <mohamed.sido.a@gmail.com>
2026-07-16 20:35:46 -0700

    Add company dataset and finish web app dependency setup

---
+++ web/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-blue-200 p-8">
+      <h1 className="text-3xl font-bold">Vite + React</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </div>
+  )
+}
+
+export default App
